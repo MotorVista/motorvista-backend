5340914a86afd1f8942b6ce41ce7a6c687c305df
mykola2312 <nikolay_tihonov@i.ua>
2023-09-12 19:14:41 +0300

    add test route to test nginx rules

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,8 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello World from TS Node Express!");
 });
 
+app.get("/test", (req: Request, res: Response) => {
+    res.send("A test route!");
+});
+
 app.listen(3000);
