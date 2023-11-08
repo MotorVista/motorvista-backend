f7c3b5734519892830ac3cb3eb2a96807101fe29
mykola2312 <nikolay_tihonov@i.ua>
2023-11-08 02:17:24 +0200

    fix tire info to more precise and described

--- migrations/20230920105609_init.sql
+++ migrations/20230920105609_init.sql
@@ -63,12 +63,12 @@ CREATE TABLE omnicars.car (
     gearbox_type omnicars.gearbox_type NOT NULL,
     gearbox_number INT,
 
-    tire_a FLOAT,
-    tire_b FLOAT,
-    tire_r SMALLINT,
+    tire_width FLOAT,
+    tire_profile FLOAT,
+    tire_rim FLOAT,
 
-    wheel_a SMALLINT,
-    wheel_b SMALLINT,
+    wheel_width FLOAT,
+    wheel_height FLOAT,
 
     CONSTRAINT car_vendor
         FOREIGN KEY(vendor) REFERENCES omnicars.vendor(id),
