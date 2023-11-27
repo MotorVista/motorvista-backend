4f0b860e1e374197e30573724c159cab07e8384b
mykola2312 <nikolay_tihonov@i.ua>
2023-11-27 15:04:33 +0200

    add body parser and session stuff

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
 import dotenv from "dotenv";
 import express, {Express, NextFunction, Request, Response} from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import sessions from "express-session";
 import "express-async-errors";
 import APIErrorHandler from './middleware/api-error-handler.js';
 import { AppDataSource } from "./data-source.js";
 import { AppRoutes } from "./routes.js";
 
 AppDataSource.initialize().then(() => {
+    dotenv.config();
     const app: Express = express();
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    app.use(sessions({
+        secret: process.env.SESSION_SECRET,
+        cookie: { maxAge: 24 * 60 * 60 * 1000 },
+        saveUninitialized: true,
+        resave: false
+    }));
+
     AppRoutes.forEach((route) => {
         app[route.method](route.path, (request: Request, response: Response, next: NextFunction) => {
             route.action(request, response)
