ecc9914965ff731ef6ef2ad9e97bd517b8338c28
mykola2312 <nikolay_tihonov@i.ua>
2023-11-27 08:38:24 +0200

    remove debug nodes

--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,18 @@ import { AppDataSource } from "./data-source.js";
 import { AppRoutes } from "./routes.js";
 
 AppDataSource.initialize().then(() => {
+    const app: Express = express();
+    AppRoutes.forEach((route) => {
+        app[route.method](route.path, (request: Request, response: Response, next: NextFunction) => {
+            route.action(request, response)
+                .then(() => next())
+                .catch(err => next(err));
+        });
+    });
+
+    app.use(APIErrorHandler);
+
     app.listen(3000);
 }).catch((e) => {
     console.log(e);
 })
-
-const app: Express = express();
-AppRoutes.forEach((route) => {
-    app[route.method](route.path, (request: Request, response: Response, next: NextFunction) => {
-        route.action(request, response)
-            .then(() => next())
-            .catch(err => next(err));
-    });
-});
-
-app.get("/", async (req: Request, res: Response) => {
-    res.send("Hello World from TS Node Express!");
-});
-
-app.get("/test", async (req: Request, res: Response) => {
-    res.send("A test route!");
-});
-
-import { errors } from "./error.js";
-app.get("/error", async (req: Request, res: Response) => {
-    throw errors.INVALID_PARAMETER;
-});
-
-app.use(APIErrorHandler);
